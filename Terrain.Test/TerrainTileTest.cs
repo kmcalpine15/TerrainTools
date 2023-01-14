feb8e02728e6d8c0307accdcc148e7c6ffe73ba5
Kris <krismcalpine@gmail.com>
2023-01-14 14:26:49 +0000

    feat: add collides with method

--- Terrain.Test/TerrainTileTest.cs
+++ Terrain.Test/TerrainTileTest.cs
@@ -11,12 +11,14 @@ namespace Terrain.Test
             float[][] data = new float[2][];
             data[0] = new float[4];
             
-            var tile = new TerrainTile(data, 50);
+            var tile = new TerrainTile(data, 50, 5, 6);
 
             Assert.AreEqual(50, tile.CellSize);
             Assert.AreEqual(data, tile.Data);
             Assert.AreEqual(2, tile.NumRows);
             Assert.AreEqual(4, tile.NumColumns);
+            Assert.AreEqual(5, tile.X);
+            Assert.AreEqual(6, tile.Y);
         }
 
 
@@ -27,7 +29,7 @@ namespace Terrain.Test
         {
             float[][] data = new float[1][];
             data[0] = testData;
-            var tile = new TerrainTile(data, 50);
+            var tile = new TerrainTile(data, 50, 1,1);
 
             var result = tile.Normalise(min, max);
 
@@ -43,10 +45,29 @@ namespace Terrain.Test
         {
             float[][] data = new float[1][];
             data[0]  = new float[3];
-             var tile = new TerrainTile(data, 50);
+            var tile = new TerrainTile(data, 50, 1, 1);
 
             Assert.ThrowsException<ArgumentException>(() => tile.Normalise(1000.0f, 500.0f));
              
+        }
+
+        [DataTestMethod]
+        [DataRow(new float[] { 0.0f, 0.0f, 0.0f, 0.0f, 0.0f }, new float[] { 0.0f, 0.0f, 0.0f, 0.0f, 0.0f }, 0,0,50,0, true)]
+        [DataRow(new float[] { 0.0f, 0.0f }, new float[] { 0.0f, 0.0f }, 0, 0, 20, 0, true)]
+        [DataRow(new float[] { 0.0f, 0.0f }, new float[] { 0.0f, 0.0f }, 0, 0, 30, 0, false)]
+        [DataRow(new float[] { 0.0f, 0.0f }, new float[] { 0.0f, 0.0f }, 0, 0, 20, 10, true)]
+        [DataRow(new float[] { 0.0f, 0.0f }, new float[] { 0.0f, 0.0f }, 20, 10, 0, 0, true)]
+        public void CollidesWith_ShouldReturnTrueForIntersectingTilesOnly(float[] tileData1, float[] tileData2, int tile1X, int tile1Y, int tile2X, int tile2Y, bool expected)
+        {
+            float[][] data1 = new float[1][];
+            data1[0] = tileData1;
+            TerrainTile tile1 = new TerrainTile(data1, 10, tile1X, tile1Y);
+
+            float[][] data2 = new float[1][];
+            data2[0] = tileData2;
+            TerrainTile tile2 = new TerrainTile(data2, 10, tile2X, tile2Y);
+
+            Assert.AreEqual(expected, tile1.CollidesWith(tile2));
         }
     }
 }
--- Terrain/Data/TerrainTile.cs
+++ Terrain/Data/TerrainTile.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Terrain.Data
+﻿namespace Terrain.Data
 {
     public class TerrainTile
     {
@@ -12,13 +6,17 @@ namespace Terrain.Data
         public int NumRows { get; private set; }
         public int CellSize { get; private set; }
         public float[][] Data { get; private set; }
+        public int X { get; private set; }
+        public int Y { get; private set; }
 
-        public TerrainTile(float[][]data, int cellSize)
+        public TerrainTile(float[][]data, int cellSize, int x, int y)
         {
             CellSize = cellSize;
             Data = data;
             NumColumns = Data[0].Length;
             NumRows = Data.Length;
+            X = x;
+            Y = y;
         }
 
         public TerrainTile Normalise(float globalMin, float globalMax)
@@ -28,8 +26,23 @@ namespace Terrain.Data
             float range = globalMax - globalMin;
             var resultData = Data.Select(row => row.Select(height => (height - globalMin) / range).ToArray()).ToArray();
 
-            return new TerrainTile(resultData, CellSize);
+            return new TerrainTile(resultData, CellSize, X, Y);
         }
         
+        public bool CollidesWith(TerrainTile other)
+        {
+            int a_left = X;
+            int a_bottom = Y;
+            int a_right = X + (CellSize * NumColumns);
+            int a_top = Y + (CellSize * NumRows);
+
+            int b_left = other.X;
+            int b_bottom = other.Y;
+            int b_right = other.X + (other.CellSize * other.NumColumns);
+            int b_top = other.Y + (other.CellSize * other.NumRows);
+
+            return (a_left <= b_right && a_right >= b_left && a_bottom <= b_top && a_top >= b_bottom);
+        }
+       
     }
 }
