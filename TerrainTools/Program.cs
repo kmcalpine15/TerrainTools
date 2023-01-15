f2d6d93cd74e15dade15364a75dbe569cbe07332
Kris <krismcalpine@gmail.com>
2023-01-15 08:17:21 +0000

    feat: add test app to read ascii grid files

--- TerrainTools/Program.cs
+++ TerrainTools/Program.cs
@@ -1,9 +1,18 @@
-﻿namespace TerrainTools
+﻿using Terrain.Data;
+using Terrain.Loaders;
+
+namespace TerrainTools
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            string filePath = "D:\\OSMapData\\temp_test\\";
+
+            AsciiGridLoader loader = new AsciiGridLoader();
+            var result = loader.LoadTerrain(filePath);
+            
+            
             Console.WriteLine("Hello, World!");
         }
     }
