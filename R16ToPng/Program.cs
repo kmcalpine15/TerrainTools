28ca1fac64c0f3e7cbbf16077a008a94e4fcec49
Kris <krismcalpine@gmail.com>
2023-01-15 10:15:19 +0000

    feat: update R16 debug helper to create png for a directory

--- R16ToPng/Program.cs
+++ R16ToPng/Program.cs
@@ -13,27 +13,38 @@ namespace R16ToPng
             int width = int.Parse(args[1]);
             int height = int.Parse(args[2]);
 
+            DirectoryInfo dir = new DirectoryInfo(filePath);
+            var files = dir.GetFiles("*.r16");
+            
+            foreach (var file in files)
+            {
+                ProcessFile(file.FullName, width, height);
+            }
+        }
+        
+        private static void ProcessFile(string filePath, int width, int height)
+        {
             short[][] data = new short[height][];
 
             using (var reader = new FileStream(filePath, FileMode.Open, FileAccess.Read))
             {
-                using(var rd = new BinaryReader(reader))
+                using (var rd = new BinaryReader(reader))
                 {
                     for (int i = 0; i < height; i++)
                     {
                         data[i] = new short[width];
                         for (int j = 0; j < width; j++)
                         {
-                            data[i][j] = (short) rd.ReadUInt16();
+                            data[i][j] = (short)rd.ReadUInt16();
                         }
                     }
                 }
             }
-            
+
 
             Bitmap img = new Bitmap(width, height, System.Drawing.Imaging.PixelFormat.Format24bppRgb);
-            
-            for(int y=0;y<height; y++)
+
+            for (int y = 0; y < height; y++)
             {
                 for (int x = 0; x < width; x++)
                 {
@@ -42,9 +53,9 @@ namespace R16ToPng
                     img.SetPixel(x, y, Color.FromArgb(col, col, col));
                 }
             }
-        
 
-            img.Save("D:\\OSMapData\\RawHeightmap\\NT.png", ImageFormat.Png);
+            string outputPath = Path.Combine(Path.GetDirectoryName(filePath), Path.GetFileNameWithoutExtension(filePath) + ".png");
+            img.Save(outputPath, ImageFormat.Png);
         }
     }
 }
